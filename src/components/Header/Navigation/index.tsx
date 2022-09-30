556ac49181b930c78fdceb587677a7a22df3d879
ddzaytsev <DDZaytsev@sberbank.ru>
2022-09-30 16:16:35 +0300

    update nav items

--- src/components/Header/Navigation/index.tsx
+++ src/components/Header/Navigation/index.tsx
@@ -1,32 +1,59 @@
 import React, {useContext} from 'react';
-import {NavLink} from "react-router-dom";
-import styled from 'styled-components'
 import {localized} from "Standard/hooks/localized";
 import LocaleContext from "Standard/LocaleContext";
 import {NavItemsEnum} from "types/NavItems";
 import texts from './localization'
+import styled from "styled-components";
+import {useHistory} from "react-router-dom";
 
 type NavItemProps = {
   title: NavItemsEnum,
   route: string
 }
 
-const StyledNavLink = styled(NavLink)`
+const Link = styled.div`
+  color: #181833;
+  text-transform: uppercase;
   font-weight: 500;
-  font-size: 16px;
-  color: rgba(23, 24, 51, .5)
+  margin-right: 50px;
+  letter-spacing: 2px;
+  position: relative;
+  transition: all 0.3s 0.2s ease;
+  cursor: pointer;
+
+  &:last-child {
+    margin-right: 0;
+  }
+
+  &:before {
+    content: '';
+    position: absolute;
+    width: 0;
+    height: 2px;
+    background: #33CC66;
+    bottom: -4px;
+    border-radius: 4px;
+    transition: all 0.4s cubic-bezier(0.82, 0.02, 0.13, 1.26);
+    left: 100%;
+  }
+
+  &:hover {
+    opacity: 1;
+    color: #33CC66;
+
+    &:before {
+      width: 40px;
+      left: 0;
+    }
+  }
 `
 
 const NavItem = (props: NavItemProps) => {
   const {title, route} = props
   const {locale} = useContext(LocaleContext)
+  const history = useHistory()
 
-  return <StyledNavLink
-    to={`${route}`}
-    activeStyle={{color: '#33CC66'}}
-  >
-    {localized(texts[title], locale)}
-  </StyledNavLink>
+  return <Link onClick={() => history.push(route)}>{localized(texts[title], locale)}</Link>
 };
 
 export default NavItem;
